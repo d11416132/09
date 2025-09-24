ea2b26c0bab9008d585cde736c07ffb5238c06da
d11416132 <d11416132@gs.takming.edu.tw>
2025-09-24 11:45:51 +0800

    1

---
+++ 2/2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ch16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+            Console.Write("請輸入身高（公分）：");
+            string inputHeight = Console.ReadLine();
+            Console.Write("請輸入體重（公斤）：");
+            string inputWeight = Console.ReadLine();
+
+            // 將輸入轉為數值
+            double height = Convert.ToDouble(inputHeight);
+            double weight = Convert.ToDouble(inputWeight);
+
+            // 計算 BMI（注意：需要把公分轉為公尺）
+            double bmi = weight / ((height / 100) * (height / 100));
+
+            // 輸出結果
+            Console.WriteLine("您的BMI值為：" + bmi.ToString("0.00"));
+        }
+    }
+}
